bb54bc7fc8dd1b45965b705d63b465c2ebe0f273
Laurintino <85128138+Laurintino@users.noreply.github.com>
2022-03-22 23:42:01 -0300

    Add files via upload

---
+++ imc2/assets/script/script.js
@@ -0,0 +1,8 @@
+function calcular(){
+    var resultpeso = parseFloat(document.querySelector("#peso").value);
+    var resultaltura = parseFloat(document.querySelector("#altura").value);
+
+    var imc = resultpeso / ( resultaltura * resultaltura);
+
+    document.querySelector(".resultado").innerHTML = `O seu IMC é ${imc.toFixed(2)}`;
+}
